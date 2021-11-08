79cfd2905a1892ae99a9cc054a1785113d7a55e3
Alexander Arthur <alex.geass@gmail.com>
2021-11-07 21:30:45 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+        <h1>Página Home</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
